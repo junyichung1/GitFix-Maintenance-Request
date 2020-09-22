6456dbff1806f6bc3e17240e704224c18f5bded6
Mustafa Hepekiz <mustafah@Mustafas-MacBook-Pro-2.local>
2020-09-21 22:16:21 -0500

    Login template and signup

--- main_app/urls.py
+++ main_app/urls.py
@@ -5,4 +5,5 @@ urlpatterns = [
     path('', views.home, name='home'),
     path('accounts/signup/', views.signup, name='signup'),
     path('tickets/', views.tickets_index, name='index'),
+    path('logout/', views.NewLogoutView.as_view()),
 ]
